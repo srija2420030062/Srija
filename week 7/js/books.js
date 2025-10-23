c6a36a91f0db456dd47f46aaf13dcc9d92fa0401
srija2420030062 <2420030062@klh.edu.in>
2025-10-23 17:59:23 +0530

    Add files via upload

---
+++ week 7/js/books.js
@@ -0,0 +1,7 @@
+// js/books.js
+export async function fetchBooks() {
+  const res = await fetch('./books.json');
+  if (!res.ok) throw new Error('Failed to load books.json');
+  return await res.json();
+}
+
